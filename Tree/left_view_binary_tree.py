32298924c97a5610d6ba36d996269a07fcadf7d2
Carlos Gil <crafaelgil@gmail.com>
2022-02-22 20:14:17 +0900

    Add new solution

--- Tree/left_view_binary_tree.py
+++ Tree/left_view_binary_tree.py
@@ -8,7 +8,7 @@ class Node:
     self.left = left
     self.right = right
 
-def left_view(root):
+def left_view_iterative(root: Node):
   if root is None:
     return
 
@@ -28,6 +28,25 @@ def left_view(root):
       if child.right:
         queue.append(child.right)
 
+def left_view_recursive(root: Node, level: int, d: dict):
+  if root is None:
+    return
+
+  if level not in d:
+    d[level] = root.data
+
+  left_view_recursive(root.left, level+1, d)
+  left_view_recursive(root.right, level+1, d)
+
+def print_left_view(root: Node):
+  d = {}
+
+  left_view_recursive(root, 1, d)
+
+  for i in range(1, len(d)+1):
+    print(d[i], end=' ')
+  print('\n')
+
 
 if __name__ == '__main__':
   root = Node(1)
@@ -39,4 +58,6 @@ if __name__ == '__main__':
   root.right.left.left = Node(7)
   root.right.left.right = Node(8)
 
-  left_view(root)
+  left_view_iterative(root)
+  print('\n')
+  print_left_view(root)
