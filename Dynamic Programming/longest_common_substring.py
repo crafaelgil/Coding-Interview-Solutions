d188ccb2032ea162f3174ec20bb97dc60383ea3a
Carlos Gil <crafaelgil@gmail.com>
2022-03-05 16:44:19 +0900

    Add new solution

---
+++ Dynamic Programming/longest_common_substring.py
@@ -0,0 +1,18 @@
+def find_longest_common_substring(X,Y):
+  table = [[0 for _ in range(len(Y)+1)] for _ in range(len(X)+1)]
+
+  result = 0
+
+  for i in range(1, len(X)):
+    for j in range(1,len(Y)):
+      if X[i-1] == Y[j-1]:
+        table[i][j] = table[i-1][j-1] + 1
+        result = max(result, table[i][j])
+
+  return result
+
+X = 'OldSite:GeeksforGeeks.org'
+Y = 'NewSite:GeeksQuiz.com'
+
+print(find_longest_common_substring(X,Y))
+print(recursive_solution(len(X), len(Y), 0))
