f3639f0b5249368a28cd46e55ab0a1829f6be83a
Carlos Gil <crafaelgil@gmail.com>
2022-03-12 17:38:24 +0900

    Add new solution

--- Tree/hight_binary_tree.py
+++ Tree/hight_binary_tree.py
@@ -1,5 +1,32 @@
-def height(self, root):
+def height_recursive(self, root):
   if root is None:
       return 0
 
   return max(self.height(root.left), self.height(root.right)) + 1
+
+def height_iteraitve(self, root):
+  if root is None:
+    return 0
+
+  level = []
+
+  level.append(root)
+  height = 0
+
+  while True:
+    num_nodes = len(level)
+    if num_nodes == 0:
+      return height
+
+    height += 1
+
+    while num_nodes > 0:
+      node = level[0]
+      level.pop(0)
+
+      if node.left is not None:
+        level.append(node.left)
+      if node.right is not None:
+        level.append(node.right)
+
+      num_nodes -= 1
