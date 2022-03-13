e86cf4e152af2d5f89f38104222273f6f43de8c5
Carlos Gil <crafaelgil@gmail.com>
2022-03-13 16:36:24 +0900

    Add new solution

--- Tree/mirror_tree.py
+++ Tree/mirror_tree.py
@@ -6,3 +6,20 @@ def mirror_tree_recursive(root):
   mirror_tree_recursive(root.right)
 
   root.left, root.right = root.right, root.left
+
+def mirror_tree_iterative(root):
+  if root is None:
+    return
+
+  level = []
+
+  while len(level) > 0:
+    node = level.pop()
+
+    if node is None:
+      continue
+
+    node.left, node.right = node.right, node.left
+
+    level.append(node.left)
+    level.append(node.right)
