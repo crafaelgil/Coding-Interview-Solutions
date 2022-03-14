a8a8f476fcb065be8d2bf651bd163db19d5a7e10
Carlos Gil <crafaelgil@gmail.com>
2022-03-14 14:13:59 +0900

    Add new solution

--- Heaps/kth_largest_numer_in_array.py
+++ Heaps/kth_largest_numer_in_array.py
@@ -1,3 +1,4 @@
+from audioop import reverse
 import heapq as hp
 
 def find_kth_largest_element(nums, k):
@@ -13,3 +14,8 @@ def find_kth_largest_element(nums, k):
     hp.heappop(nums)
 
   return nums[0]
+
+def simpler_solution(nums, k):
+  nums.sort(reverse=True)
+
+  return nums[k-1]
